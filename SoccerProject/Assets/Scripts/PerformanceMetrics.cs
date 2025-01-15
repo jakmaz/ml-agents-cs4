e5e3fc7753eb3caf80bfc4e35ed509eb6dc2ecce
Stanimira21 <stanimira.dimitrova04@gmail.com>
2025-01-15 15:52:27 +0100

    added memory, frane, and CPU metrics

--- SoccerProject/Assets/Scripts/PerformanceMetrics.cs
+++ SoccerProject/Assets/Scripts/PerformanceMetrics.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.Profiling;
 
 public class PerformanceMetrics{
     public Team? Winner { get; set; }
@@ -7,4 +8,7 @@ public class PerformanceMetrics{
     public float BluePenalties { get; set; }
     public float PurpleRewards { get; set; }
     public float PurplePenalties { get; set; }
+    public float AverageFrameRate { get; set; } 
+    public float AverageCPUUsage { get; set; }  
+    public long MemoryUsage { get; set; }       
 }
