09f23658b4230221fc4946d8c476efdefc9315b0
redstarcomets <v.shabanaj@student.maastrichtuniversity.nl>
2025-01-19 20:08:07 +0100

    Fixed up ultimate and fouls model + retrained (FA_SoccerTwos.onnx and FF_SoccerTwos.onnx)

--- SoccerProject/Assets/Scripts/AgentSoccer.cs
+++ SoccerProject/Assets/Scripts/AgentSoccer.cs
@@ -381,11 +381,11 @@ public class AgentSoccer : Agent
             // Penalize for colliding with opposing players
             if (team == Team.Blue && c.gameObject.CompareTag("purpleAgent"))
             {
-                AddReward(-0.01f); // Penalize blue agent for colliding with a purple agent
+                AddReward(-0.002f); // Penalize blue agent for colliding with a purple agent
             }
             else if (team == Team.Purple && c.gameObject.CompareTag("blueAgent"))
             {
-                AddReward(-0.01f); // Penalize purple agent for colliding with a blue agent
+                AddReward(-0.002f); // Penalize purple agent for colliding with a blue agent
             }
         }
     }
